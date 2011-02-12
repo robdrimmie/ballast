5fb8b51bffc4b987f27acb794d7a3eed2feeb5d2
rmdrimmie <rob.drimmie@gmail.com>
2011-02-12 16:43:28 -0500

    Initial install. Redoing ballast with start from http://railstutorial.org/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ballast::Application.config.secret_token = 'dfab99af2a4edb7e13894cff318654b6cf4a618af7312ee28a053480123668040fbb3169a4de1695f69467d1a6d5f42b140522378974ffe0aabdbb13204cf97c'
