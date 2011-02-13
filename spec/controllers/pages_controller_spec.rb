a5cf8fd51205307e4fa75a330c631a79c407ee78
rmdrimmie <rob.drimmie@gmail.com>
2011-02-13 07:21:37 -0500

    Fixed error in test file

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_select("title",
+      response.should have_selector("title",
                                   :content => "Ruby on Rails Tutorial Sample App | Home" )
     end
 
@@ -25,7 +25,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_select("title",
+      response.should have_selector("title",
                                   :content => "Ruby on Rails Tutorial Sample App | Contact" )
     end
   end
@@ -38,7 +38,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_select("title",
+      response.should have_selector("title",
                                   :content => "Ruby on Rails Tutorial Sample App | About" )
     end
   end
