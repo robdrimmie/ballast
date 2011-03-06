83b8b9ee44d4af6588ee4b5c4478d3a5a1b15b85
rmdrimmie <rob.drimmie@gmail.com>
2011-03-05 20:12:40 -0500

    Not really changes, but something's borken with signins I want to fix.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Thank you, and welcome to Ballastline!"
       redirect_to @user
     else
       @title = "Sign up"
